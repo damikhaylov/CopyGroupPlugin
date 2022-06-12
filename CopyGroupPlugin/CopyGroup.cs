d97eed9d72044e1bbfa033d21a273de1bdc6edc9
Denis Mikhaylov <dnmh@yandex.ru>
2022-06-12 20:05:40 +0700

    feat: добавить обработку ошибок и вставку группы по центру комнаты

--- CopyGroupPlugin/CopyGroup.cs
+++ CopyGroupPlugin/CopyGroup.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -16,21 +17,86 @@ namespace CopyGroupPlugin
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
-            Document doc = uiDoc.Document;
-            Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов");
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
 
-            Element element = doc.GetElement(reference);
-            Group group = element as Group;
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, groupPickFilter, "Выберите группу объектов");
 
-            XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+                Element element = doc.GetElement(reference);
+                Group group = element as Group;
+                XYZ groupCenter = GetElementCenter(group);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                if (null == room)
+                {
+                    message = "Группа не находится внутри комнаты";
+                    return Result.Failed;
+                }
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
 
-            Transaction transaction = new Transaction(doc);
-            transaction.Start("Копирование группы объектов");
-            doc.Create.PlaceGroup(point, group.GroupType);
-            transaction.Commit();
+                XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+                Room newRoom = GetRoomByPoint(doc, point);
+                if (null == newRoom)
+                {
+                    message = "Указана точка за пределами комнаты";
+                    return Result.Failed;
+                }
+                XYZ newRoomCenter = GetElementCenter(newRoom);
+                XYZ newGroupCenter = newRoomCenter + offset;
+
+                Transaction transaction = new Transaction(doc);
+                transaction.Start("Копирование группы объектов");
+                doc.Create.PlaceGroup(newGroupCenter, group.GroupType);
+                transaction.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
 
             return Result.Succeeded;
+        }
+
+        public XYZ GetElementCenter(Element element)
+        {
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element e in collector)
+            {
+                Room room = e as Room;
+                if (room != null && room.IsPointInRoom(point))
+                {
+                    return room;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
         }
     }
 }
